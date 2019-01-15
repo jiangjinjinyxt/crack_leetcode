339f2d21d1317643ca7e88d859c7e5bc62528f38
JJJ <jiangjinjin@JJJdeMacBook-Pro.local>
2019-01-15 19:34:07 +0800

    P3

--- P3.py
+++ P3.py
@@ -12,3 +12,19 @@ class Solution:
         :type s: str
         :rtype: int
         """
+        previous_index = -1
+        look_up = dict()
+        max_len = 0
+        for idx, value in enumerate(s):
+            if value in look_up:
+                if previous_index < look_up[value]:
+                    previous_index = look_up[value]
+                else:
+                    temp_max = idx - previous_index
+                    if temp_max > max_len:
+                        max_len = temp_max
+            else:
+                temp_max = idx - previous_index
+                if temp_max > max_len:
+                    max_len = temp_max
+        return max_len
