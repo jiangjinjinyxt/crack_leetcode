69ce192d8f5dcb7efea8d83c57b7939b31bd287c
JJJ <jiangjinjinyxt@github.com>
2019-03-01 21:26:27 +0800

    2019-03-01 P0179.py

---
+++ P0179.py
@@ -0,0 +1,17 @@
+"""
+problem 179: Largest Number
+https://leetcode.com/problems/largest-number/
+
+solution:
+
+"""
+class LargerNumKey(str):
+    def __lt__(x, y):
+        return x+y > y+x
+        
+class Solution:
+    def largestNumber(self, nums):
+        largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
+        return '0' if largest_num[0] == '0' else largest_num
+
+        
