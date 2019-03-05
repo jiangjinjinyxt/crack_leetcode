a381dcb16859c9ce7490b83c3786d1e93bbbf7f1
JJJ <jiangjinjinyxt@github.com>
2019-03-05 22:45:26 +0800

    2019-03-05 P0238.py

---
+++ P0238.py
@@ -0,0 +1,14 @@
+"""
+Problem 238. Product of Array Except Self
+https://leetcode.com/problems/product-of-array-except-self/
+
+solution:
+
+"""
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
